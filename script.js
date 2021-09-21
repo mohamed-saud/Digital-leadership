4c9873d797c0bfae7cf1e8dcc23d68c38c223503
mohamed-saud <mo7amed.saud2020@gmail.com>
2021-09-21 16:13:25 +0200

    finsh project

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
   $("#burger").on("click", function () {
-    $(".links").addClass("open-side");
+    $(".links").toggleClass("open-side");
+    $(this).toggleClass("anmat-burger");
   });
-  $("#closse-side").on("click", function () {
-    $(".links").removeClass("open-side");
-    $(this).animate(
-      {
-        boxShadow: "5px 10px 30px 30px #720707a1",
-        // background: "red",
-      },
-      100
-    );
-  });
+  // $("#closse-side").on("click", function () {
+  //   $(".links").removeClass("open-side");
+  //   $(".closse").animate("border", " 2px solid white");
+  // });
   $(".right-colo").click(() => {
     $(".obturateur").animate(
       {
@@ -28,13 +23,20 @@ $(document).ready(function () {
   });
 
   $("#fullpage").fullpage({
-    //options here
-    anchors: ["Page1", "Page2", "Page3", "Page4", "Page5"],
+    anchors: [
+      "Page1",
+      "Page2",
+      "Page3",
+      "Page4",
+      "Page5",
+      "Page6",
+      "Page7",
+      "Page8",
+    ],
     afterLoad: function (origin, destination, direction) {
       var loadedSection = this;
       var imgSrc = $("#img-src");
 
-      //usando su índice
       if (destination.anchor == "Page1") {
         imgSrc.attr(
           "src",
@@ -42,7 +44,6 @@ $(document).ready(function () {
         );
       }
 
-      //usando su link de anclaje
       if (destination.anchor == "Page2") {
         imgSrc.attr(
           "src",
@@ -56,11 +57,34 @@ $(document).ready(function () {
         );
       }
 
-      //usando su link de anclaje
       if (destination.anchor == "Page4") {
         imgSrc.attr(
           "src",
           "https://una.era-solutions.com/stefan-book/wp-content/uploads/2021/08/digital-leadership_logo_standard-logo_white.svg"
+        );
+      }
+      if (destination.anchor == "Page5") {
+        imgSrc.attr(
+          "src",
+          "https://una.era-solutions.com/stefan-book/wp-content/uploads/2021/08/digital-leadership_logo_standard-logo_white.svg"
+        );
+      }
+      if (destination.anchor == "Page6") {
+        imgSrc.attr(
+          "src",
+          "https://una.era-solutions.com/stefan-book/wp-content/uploads/2021/08/digital-leadership_logo_standard-logo_white.svg"
+        );
+      }
+      if (destination.anchor == "Page7") {
+        imgSrc.attr(
+          "src",
+          "https://una.era-solutions.com/stefan-book/wp-content/uploads/2021/08/digital-leadership_logo_standard-logo_white.svg"
+        );
+      }
+      if (destination.anchor == "Page8") {
+        imgSrc.attr(
+          "src",
+          "https://una.era-solutions.com/stefan-book/wp-content/uploads/2021/08/digital-leadership_logo_standard-logo_black.svg"
         );
       }
     },
@@ -79,4 +103,21 @@ $(document).ready(function () {
   //methods
 
   fullpage_api.setAllowScrolling(true);
+
+  ///////slider method
+  function slider(time, parantClass, elment, classAct) {
+    setInterval(() => {
+      if ($(`.${parantClass} .${elment}.${classAct}`).is(":last-child")) {
+        $(`.${parantClass} .${elment}:nth-child(1)`).addClass(classAct);
+      }
+      $(`.${parantClass} .${elment}.${classAct}`)
+        .removeClass(classAct)
+        .next()
+        .addClass(classAct);
+    }, time);
+  }
+
+  slider(2000, "sliderss", "slidee", "slected");
+  slider(2000, "navigation-auto", "auto-btn", "active");
+  slider(2000, "slider-contaner", "slid-item", "slected");
 });
